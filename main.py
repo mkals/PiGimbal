b034144fbf65c8d2f2599a55d9f70328a43d6cac
Morten Kals <m.kals@me.com>
2016-09-03 23:15:16 -0700

    outlines of main in order

--- main.py
+++ main.py
@@ -4,34 +4,68 @@ class Mode(Enum):
     video = 0
     image = 1
     timelapse = 2
-    faceTracking = 3
-    objectTracking = 4
+#    faceTracking = 3
+#    objectTracking = 4
 
 
 from RPi import GPIO
 
-from video import VideoCapture
-from switch import Switch
-from pwm import Pwm
-from led import Led
+from myCamera import Camera
+from mySwitch import Switch
+from myPWM import PWM
+from myLED import LED
 
-GPIO.setmode......
+GPIO.setmode(GPIO.BCM)
 
-video = VideoCapture()
+
+camera = Camera()
 recordSwitch = Switch(12)
 menuSwitch = Switch(14)
-led = Led(14)
+led = LED(14)
 
 recording = False
 mode = Mode.video
 
-while True
+while True:
     
-    if menuSwitch.activated():
-
+    if not recording and menuSwitch.activated():
+        if mode == Mode.video:
+            mode = Mode.image
+        elif mode == Mode.image:
+            mode = Mode.timelapse
+        elif mode == Mode.timelapse:
+#            mode = Mode.faceTracking
+#        elif mode == Mode.faceTracking:
+#           mode = Mode.objectTracking
+#        else:
+            mode = Mode.video
 
     if recordSwitch.activated():
+
+        if mode == Mode.video:
+            if recording:
+                camera.stopRecording()
+                recording = False
+            else:
+                camera.startRecording()
+                recording = True
+
+        elif mode == Mode.image():
+            camera.takePhoto()
+
+        elif mode == Mode.timelapse:
+            if recording:
+                camera.stopTimelapse()
+                recording = False
+            else:
+                camera.startTimelapse()
+                recording = True
+
+#        elif mode == Mode.faceTracking:
+
+#        elif mode == Mode.objectTracking:
         
+            
     
 
-GPIO.cleanup.....
+#GPIO.cleanup.....
